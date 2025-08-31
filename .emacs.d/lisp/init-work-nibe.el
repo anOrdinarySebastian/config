111c8e5e72cd6aa3bd75da57e95241a575b0bf6b
Sebastian Nirvin <sebastian.bengtsson96@gmail.com>
2025-08-31 22:37:35 +0200

    Add work-init for Nibe
    Made a init file for working at Nibe. Mainly containing the org-mode
    setup that I need for taking notes and journaling.

---
+++ .emacs.d/lisp/init-work-nibe.el
@@ -0,0 +1,142 @@
+;; This is a file with configuration related to my workplace
+
+;; Nice to have for triggering stuff that are unique for this job PC
+(setq computer-name "work-pc")
+
+;; Org mode modifications
+
+(defface org-in-review
+    '((((class color) (min-colors 88) (background light))
+       :background "darkseagreen2")
+      (((class color) (min-colors 88) (background dark))
+       :foreground "yellow3"))
+    "Face for TODO-tasks tagged with IN-PROGRESS"
+    :group 'org-faces)
+
+  ;; Yanky way of writing text return functions for
+  ;; `org-capture-templates' to have a cleaner alist
+  (defun start-day-template () nil
+         "\
+* [%<%H:%M>] Started [/] %(make-string 44 ?-)
+
+Checklist
+- [ ] Mail/Meetings
+- [ ] Jira
+"
+  (defun journal-quit-report ()
+    "Check if clock is running, clock out if it is and then print the
+clock report from the agenda files and quit"
+    (if (org-clock-is-active)
+        (org-clock-out))
+    (let* ((org-clock-clocktable-default-properties
+            '(:scope agenda :maxlevel 4 :block today :properties ("PROJECT") :fileskip0 t)))
+      (org-clock-report)))
+
+  (defun general-log-template ()
+    "Prompt for a log message and a project to report time on.
+If a task is currently clocked, add it at the top of the capture.
+If the region is active then paste it into the capture and,
+finally, put the point just under the PROPERTY drawer"
+    (let ((include-clock-link ""))
+      (if (org-clock-is-active)
+          (setq include-clock-link "Clocked in on: %K\n"))
+      (format
+       "\
+* [%%<%%H:%%M>] %%^{Log message: |Log}
+%%^{PROJECT}p%s
+%%?
+%%i"
+       include-clock-link)))
+
+(customize-set-variable
+ 'org-capture-templates '(("t" "Todo")
+                          ("tt" "New"
+                           entry
+                           (file+headline org-default-todo-file "Tasks")
+                           "* TODO %?\n  %i\n  %a")
+                          ("te" "Edit"
+                           plain
+                           (file+function org-default-todo-file org-goto)
+                           "%?\n  %i"
+                           :empty-lines 1
+                           :unnarrowed t)
+
+                          ("n" "Notes"
+                           plain
+                           (file+function org-default-notes-file org-goto)
+                           "%?")
+
+                          ("c" "Clocking"
+                           plain
+                           (clock)
+                           "%?"
+                           :unnarrowed t)
+
+                          ("j" "Journal")
+
+                          ("js" "Start day"
+                           entry
+                           (file+olp+datetree org-default-journal-file)
+                           (function start-day-template)
+                           :clock-in t
+                           :unnarrowed t
+                           :after-finalize (lambda ()
+                                             (interactive )
+                                             (org-agenda nil "i")))
+
+                          ("jq" "Quit day"
+                           entry
+                           (file+olp+datetree org-default-journal-file)
+                           "* [%<%H:%M>] Quit\n%(journal-quit-report)"
+                           :immediate-finish t)
+
+                          ("jp" "Pause"
+                           entry
+                           (file+olp+datetree org-default-journal-file)
+                           "* [%<%H:%M>] Paused\nGoing for %?"
+                           :clock-in t
+                           :after-finalize (lambda ()
+                                             (interactive )
+                                             (org-agenda nil "i")))
+
+                          ("jl" "General Log"
+                           entry
+                           (file+olp+datetree org-default-journal-file)
+                           (function general-log-template))
+
+                          ("jm" "Meeting"
+                           entry
+                           (file+olp+datetree org-default-journal-file)
+                           "* [%<%H:%M>][%^{Minutes}m] %?meeting\n%^{TOPIC}p%^{PROJECT}p"))
+ "Set up the org capture templates to fit work")
+
+(customize-set-variable
+ 'org-agenda-custom-commands '(("i" "Agenda and Jira"
+                                ((todo "IN-PROGRESS"
+                                       ((org-agenda-overriding-header "Started Jiras")))
+                                 (agenda "")
+                                 (tags-todo "meeting"
+                                            ((org-agenda-overriding-header "Meetings to plan"))))))
+ "Set up an agenda view that fits work")
+
+(use-package org-jira
+  :ensure t
+  :after org
+  :defines
+  org-default-jira-files
+  :custom
+  (org-agenda-files (nconc org-agenda-files (list org-default-jira-files)))
+  (org-jira-working-dir org-default-jira-files)
+  (jiralib-url "")
+  :init
+  (defcustom org-default-jira-files (concat org-directory "/jira-api")
+    "File to note down information about specific jira issues"
+    :type '(file :must-match t)
+    :group 'org-capture)
+  :config
+  (defconst org-jira-progress-issue-flow
+    '(("TODO" . "Selected for Development")
+      ("Selected for Development" . "In Progress")
+      ("In Progress" . "In Review")
+      ("In Review" . "Done"))))
+
